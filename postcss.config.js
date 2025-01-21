a3d7f97b995405fb14e277c2ecae59a66a3095df
okta maulana <okta.maulana12@gmail.com>
2025-01-22 02:10:45 +0700

    few update

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,28 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: ["./public/**/*.html", "./src/**/*.js"],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+            safelist: ["dark", "html", "body"],
+          },
+          cssnano: {
+            preset: [
+              "default",
+              {
+                discardComments: {
+                  removeAll: true,
+                },
+                normalizeWhitespace: true,
+                minifySelectors: true,
+                minifyFontValues: true,
+              },
+            ],
+          },
+        }
+      : {}),
   },
-}
+};
